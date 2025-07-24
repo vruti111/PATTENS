1ba07dd14ecc08b2a95ad53e61573f76ccfb4afd
Vrutii <vrutip670@gmail.com>
2025-07-24 11:10:36 +0530

    swap method varible and ref

---
+++ swap2.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+public class swap2
+{
+    static void swap(ref int a, ref int b)// a and b both are local variable
+    {
+        int temp = a;
+        a = b;
+        b = temp;
+        //a = a + b; // a = 30
+        //b = b - a; // b = 10
+    }
+    public static void Main(string[] args)
+    {
+        int a = 10, b = 20;
+        Console.WriteLine("Before swap : a = {0} and b = {1}", a, b);
+        swap(a, b);//passing the arguments by value
+        swap(ref a, ref b);//passing the arguments by value
+        Console.WriteLine("After swap : a = {0} and b = {1}", a, b);
+    }
+}
